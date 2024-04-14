c793f348276e478e6ea10e6273b0cbcea5afffb7
KarishmaGR <karishmasvryadav@gmail.com>
2024-04-15 00:01:48 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 import Express from "express";
 import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
 
 import cors from "cors";
 const app = Express();
 
 app.use(
   cors({
-    origin: "https://job-board-frontend-teal.vercel.app",
-    credentials: false,
+    origin: true,
+    credentials: true,
   })
 );
 
